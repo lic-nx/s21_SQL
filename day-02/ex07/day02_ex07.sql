bd23581a97789255bbc761e96d1bd1d9c569794d
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:31:35 +0300

    Update day02_ex07.sql

--- day-02/ex07/day02_ex07.sql
+++ day-02/ex07/day02_ex07.sql
@@ -1,3 +1,5 @@
+--Please find the name of pizzeria Dmitriy visited on January 8, 2022 and could eat pizza for less than 800 rubles.
+
 SELECT pz.name from pizzeria pz
     join person_visits pv on pv.pizzeria_id = pz.id
     join person p on p.id = pv.person_id
