04b0b2323014736f53fad4f6bdd2b2c7a5f07334
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:21:12 +0300

    Update day01_ex09.sql

--- day-01/ex09/day01_ex09.sql
+++ day-01/ex09/day01_ex09.sql
@@ -1,3 +1,4 @@
+-- Please write 2 SQL statements which return a list of pizzerias names which have not been visited by persons by using IN for 1st one and EXISTS for the 2nd one.
 SELECT name from pizzeria where id not in (SELECT pizzeria_id from person_visits );
 
 SELECT name from pizzeria where NOT EXISTS(SELECT pizzeria_id from person_visits where pizzeria_id = pizzeria.id);
