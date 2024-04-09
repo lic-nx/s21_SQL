994ba4a1cc93f6c54e4372a75fe757f27064d754
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:35:41 +0300

    Update day03_ex04.sql

--- day-03/ex04/day03_ex04.sql
+++ day-03/ex04/day03_ex04.sql
@@ -1,4 +1,4 @@
-
+-- Please find a union of pizzerias that have orders either from women or  from men. Other words, you should find a set of pizzerias names have been ordered by females only and make "UNION" operation with set of pizzerias names have been ordered by males only. Please be aware with word “only” for both genders. For any SQL operators with sets don’t save duplicates (UNION, EXCEPT, INTERSECT).  Please sort a result by the pizzeria name. The data sample is provided below.
 with womenOrders as (
     select m.pizzeria_id  from person
                         join person_order po on person.id = po.person_id
