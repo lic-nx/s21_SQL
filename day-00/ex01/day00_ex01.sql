772589df947ae734c8f1c0955c10c5e1262f0c9a
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:13:33 +0300

    Update day00_ex01.sql

--- day-00/ex01/day00_ex01.sql
+++ day-00/ex01/day00_ex01.sql
@@ -1,1 +1,2 @@
+-- Please make a select statement which returns names , ages for all women from the city ‘Kazan’. Yep, and please sort result by name.
 SELECT name, age FROM person WHERE address = 'Kazan' AND  gender = 'female' ORDER BY name;
