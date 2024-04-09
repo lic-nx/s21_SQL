ff579d67f32f344e923fecfa112b7cef43c5db6a
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:34:42 +0300

    Update day03_ex02.sql

--- day-03/ex02/day03_ex02.sql
+++ day-03/ex02/day03_ex02.sql
@@ -1,3 +1,4 @@
+-- Please use SQL statement from Exercise #01 and show pizza names from pizzeria which are not ordered by anyone, including corresponding prices also. The result should be sorted by pizza name and price. The sample of output data is presented below.
 SELECT pizza_name, price, pz.name from menu
     join pizzeria pz on pz.id = menu.pizzeria_id
  where menu.id not in (SELECT menu_id from person_order)
