50ccb1cc850d17a5e7efaf2bb0b9199ccae71348
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:36:05 +0300

    Update day03_ex05.sql

--- day-03/ex05/day03_ex05.sql
+++ day-03/ex05/day03_ex05.sql
@@ -1,3 +1,5 @@
+-- Please write a SQL statement which returns a list of pizzerias which Andrey visited but did not make any orders. Please order by the pizzeria name. The sample of data is provided below.
+
 SELECT pz.name as pizzeria_name from pizzeria pz
     join person_visits pv on pz.id = pv.pizzeria_id
     join person p on pv.person_id = p.id
