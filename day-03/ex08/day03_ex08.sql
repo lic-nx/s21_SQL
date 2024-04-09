467cdba446a8e2a337dbe27325c12d58bee4a1c3
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:37:20 +0300

    Update day03_ex08.sql

--- day-03/ex08/day03_ex08.sql
+++ day-03/ex08/day03_ex08.sql
@@ -1,2 +1,4 @@
+-- Please register a new pizza with name “sicilian pizza” (whose id should be calculated by formula is “maximum id value + 1”) with a price of 900 rubles in “Dominos” restaurant (please use internal query to get identifier of pizzeria).
+Warning: this exercise will probably be the cause  of changing data in the wrong way. Actually, you can restore the initial database model with data from the link in the “Rules of the day” section and replay script from Exercise 07.
 INSERT INTO menu
 values ((SELECT max(id) from menu)+1,(Select id from pizzeria where name = 'Dominos'), 'sicilian pizza', 900 );
