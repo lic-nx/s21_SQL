f476252f5fea2b92c420003f4b511f0e47b1ad63
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:34:59 +0300

    Update day03_ex03.sql

--- day-03/ex03/day03_ex03.sql
+++ day-03/ex03/day03_ex03.sql
@@ -1,3 +1,5 @@
+-- Please find pizzerias that have been visited more often by women or by men. For any SQL operators with sets save duplicates (UNION ALL, EXCEPT ALL, INTERSECT ALL constructions). Please sort a result by the pizzeria name. The data sample is provided below.
+
 SELECT pz.name from pizzeria pz
 join
 (select count(person.id), pv.pizzeria_id  from person
