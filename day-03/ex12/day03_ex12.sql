7c68222eb7d47c24ce1b0d7049c8145865ea1340
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:39:23 +0300

    Update day03_ex12.sql

--- day-03/ex12/day03_ex12.sql
+++ day-03/ex12/day03_ex12.sql
@@ -1,3 +1,9 @@
+--Don’t use window functions like ROW_NUMBER( )
+
+-- Don’t use atomic INSERT statements |
+
+-- Please register new orders from all persons for “greek pizza” on 25th of February 2022.
+-- Warning: this exercise will probably be the cause  of changing data in the wrong way. Actually, you can restore the initial database model with data from the link in the “Rules of the day” section and replay script from Exercises 07 , 08 ,09 , 10 and 11.
 INSERT INTO person_order
 SELECT
     pers_id+(SELECT max(id) from person_order), id , (select id from menu where pizza_name = 'greek pizza') as menu_id,
