99e4adf4a40cf6bdfec32046ed89379d7fbcda17
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:39:01 +0300

    Update day03_ex11.sql

--- day-03/ex11/day03_ex11.sql
+++ day-03/ex11/day03_ex11.sql
@@ -1,3 +1,6 @@
+--Please change the price for “greek pizza” on -10% from the current value.
+-- Warning: this exercise will probably be the cause  of changing data in the wrong way. Actually, you can restore the initial database model with data from the link in the “Rules of the day” section and replay script from Exercises 07 , 08 ,09 and 10.
+
 update menu
       set price = price-price*0.1
       where pizza_name ='greek pizza'
