ca3491a8ebdf6e955986ef2cfc2b1a257b2a1b15
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:20:05 +0300

    Update day01_ex05.sql

--- day-01/ex05/day01_ex05.sql
+++ day-01/ex05/day01_ex05.sql
@@ -1,2 +1,5 @@
+--Please write a SQL statement which returns all possible combinations between person and pizzeria tables and please set ordering by person identifier and then by pizzeria identifier columns. Please take a look at the result sample below. Please be aware column's names can be different for you.
+
+
 SELECT person.id, person.name, person.age,person.gender, person.address, pizzeria.id, pizzeria.name, pizzeria.rating from  person, pizzeria
                                                                                                                      order by person.id, pizzeria.id;
