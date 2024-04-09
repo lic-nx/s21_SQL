4111c7a7b3f85a1047bb9b4a66291c55707fef45
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:32:43 +0300

    Update day02_ex10.sql

--- day-02/ex10/day02_ex10.sql
+++ day-02/ex10/day02_ex10.sql
@@ -1,3 +1,5 @@
+-- Please find the names of persons who live on the same address. Make sure that the result is ordered by 1st person, 2nd person's name and common address. The  data sample is presented below. Please make sure your column names are corresponding column names below.
+
 SELECT  p.name as person_name1, p1.name as person_name2, p.address as common_address from person p
     join person p1 on p1.address = p.address
 where p.id > p1.id
