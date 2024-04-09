902c98d47b3d464172810aa20ca7338ff251c186
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:30:30 +0300

    Update day02_ex05.sql

--- day-02/ex05/day02_ex05.sql
+++ day-02/ex05/day02_ex05.sql
@@ -1,3 +1,5 @@
+--Find names of all female persons older than 25 and order the result by name. The sample of output is presented below.
+
 SELECT name from person
 where age > 25 and gender='female'
 order by name;
