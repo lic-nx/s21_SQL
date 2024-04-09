a57d392b3cdd592244fd6819ec00c1b8dcffb555
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:14:42 +0300

    Update day00_ex04.sql

--- day-00/ex04/day00_ex04.sql
+++ day-00/ex04/day00_ex04.sql
@@ -1,2 +1,7 @@
+-- Please make a select statement which returns one calculated field with name ‘person_information’ in one string like described in the next sample:
+-- Anna (age:16,gender:'female',address:'Moscow')
+-- Finally, please add the ordering clause by calculated column in ascending mode.
+-- Please pay attention to quote symbols in your formula!
+
 SELECT name || ' (age:'|| age|| ',gender:'|| '''' || person.gender || '''' || ',address:'||'''' || address||'''' || ')'  AS person_information FROM person
 order by person_information ASC;
