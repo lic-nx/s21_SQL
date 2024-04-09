39a45ad149b7e82b7a151cc7017cc9fbe8345ed2
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:22:05 +0300

    Update day02_ex00.sql

--- day-02/ex00/day02_ex00.sql
+++ day-02/ex00/day02_ex00.sql
@@ -1,3 +1,5 @@
+--Please write a SQL statement which returns a list of pizzerias names with corresponding rating value which have not been visited by persons.
+
 SELECT name, rating from pizzeria
 left JOIN person_visits pv on pv.pizzeria_id = pizzeria.id
 where pv.pizzeria_id is NULL;
