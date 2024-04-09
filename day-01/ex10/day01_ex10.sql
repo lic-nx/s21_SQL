c6d39065ed3d2ea0202cc36e05d6b1952844b41c
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:21:29 +0300

    Update day01_ex10.sql

--- day-01/ex10/day01_ex10.sql
+++ day-01/ex10/day01_ex10.sql
@@ -1,3 +1,7 @@
+--Please write a SQL statement which returns a list of the person names which made an order for pizza in the corresponding pizzeria.
+-- The sample result (with named columns) is provided below and yes ... please make ordering by 3 columns (person_name, pizza_name, pizzeria_name) in ascending mode.
+
+
 SELECT person.name as person_name, menu.pizza_name as pizza_name, pizzeria.name as pizzeria_name from person_order
     inner join person on person_order.person_id = person.id
     inner join menu on menu.id = person_order.menu_id
