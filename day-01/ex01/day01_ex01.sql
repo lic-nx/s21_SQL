608ca2bada3ff37995ff008c693a791f02cc30db
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:18:43 +0300

    Update day01_ex01.sql

--- day-01/ex01/day01_ex01.sql
+++ day-01/ex01/day01_ex01.sql
@@ -1,3 +1,5 @@
+--Please modify a SQL statement from “exercise 00” by removing the object_id column. Then change ordering by object_name for part of data from the person table and then from menu table (like presented on a sample below). Please save duplicates!
+
 (SELECT name as object_name  from person order by  object_name)
 UNION all
 (SELECT pizza_name as object_name from menu
