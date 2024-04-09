243c7bdef220812173fdd3a919a16c99aadad912
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:33:55 +0300

    Update day03_ex00.sql

--- day-03/ex00/day03_ex00.sql
+++ day-03/ex00/day03_ex00.sql
@@ -1,4 +1,5 @@
-  SELECT pizza_name, price, pz.name,visit_date from menu
+-- Please write a SQL statement which returns a list of pizza names, pizza prices, pizzerias names and dates of visit for Kate and for prices in range from 800 to 1000 rubles. Please sort by pizza, price and pizzeria names. Take a look at the sample of data below.
+SELECT pizza_name, price, pz.name,visit_date from menu
     join pizzeria pz on pz.id=menu.pizzeria_id
     join person_visits pv on pv.pizzeria_id = pz.id
     join person p on p.id = pv.person_id
