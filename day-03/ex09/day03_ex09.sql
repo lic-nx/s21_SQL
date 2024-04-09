a02d2a9d4a8127101b7f10c6d4806552a2d6214a
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:37:59 +0300

    Update day03_ex09.sql

--- day-03/ex09/day03_ex09.sql
+++ day-03/ex09/day03_ex09.sql
@@ -1,3 +1,6 @@
+--Please register new visits into Dominos restaurant from Denis and Irina on 24th of February 2022.
+-- Warning: this exercise will probably be the cause  of changing data in the wrong way. Actually, you can restore the initial database model with data from the link in the “Rules of the day” section and replay script from Exercises 07 and 08..
+
 INSERT INTO person_visits
 values ((SELECT max(id) from person_visits)+1, (SELECT id from person where name = 'Denis'),(Select id from pizzeria where name = 'Dominos'), '2022-02-24'),
        ((SELECT max(id) from person_visits)+2, (SELECT id from person where name = 'Irina'),(Select id from pizzeria where name = 'Dominos'), '2022-02-24');
