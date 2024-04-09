6319f4cb2122bbce760cb583e4efb1d826f10c50
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:34:21 +0300

    Update day03_ex01.sql

--- day-03/ex01/day03_ex01.sql
+++ day-03/ex01/day03_ex01.sql
@@ -1,3 +1,5 @@
+-- Please find all menu identifiers which are not ordered by anyone. The result should be sorted by identifiers. The sample of output data is presented below.
+
 SELECT id as menu_id from menu
  where id not in (SELECT menu_id from person_order)
 order by 1;
