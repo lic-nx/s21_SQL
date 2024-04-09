9e6c5fa953d17ed0ceca090f1f1715a1412387e7
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:30:13 +0300

    Update day02_ex04.sql

--- day-02/ex04/day02_ex04.sql
+++ day-02/ex04/day02_ex04.sql
@@ -1,3 +1,5 @@
+--Find full information about all possible pizzeria names and prices to get mushroom or pepperoni pizzas. Please sort the result by pizza name and pizzeria name then. The result of sample data is below (please use the same column names in your SQL statement).
+
 SELECT pizza_name, pizzeria.name, price from menu
 join pizzeria on menu.pizzeria_id= pizzeria.id
 where pizza_name='mushroom pizza' or pizza_name='pepperoni pizza'
