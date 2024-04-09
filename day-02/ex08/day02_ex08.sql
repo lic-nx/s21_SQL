6fa154efb3b9e76ddc25a69fe63b6ebeaf25b5f1
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:31:56 +0300

    Update day02_ex08.sql

--- day-02/ex08/day02_ex08.sql
+++ day-02/ex08/day02_ex08.sql
@@ -1,3 +1,5 @@
+--Please find the names of all males from Moscow or Samara cities who orders either pepperoni or mushroom pizzas (or both) . Please order the result by person name in descending mode. The sample of output is presented below.
+
 SELECT p.name from person p
               join person_order po on p.id = po.person_id
               join menu m on m.id = po.menu_id
