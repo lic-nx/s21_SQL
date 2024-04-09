2bf579f0a61660276109bc6c93baa66ee2885693
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:20:21 +0300

    Update day01_ex06.sql

--- day-01/ex06/day01_ex06.sql
+++ day-01/ex06/day01_ex06.sql
@@ -1,3 +1,7 @@
+--Let's return our mind back to exercise #03 and change our SQL statement to return person names instead of person identifiers and change ordering by action_date in ascending mode and then by person_name in descending mode. Please take a look at a data sample below.
+
+
+
 SELECT order_date as action_date, (SELECT name from person where person.id=person_order.person_id) as person_name from person_order
 INTERSECT all
 SELECT visit_date as action_date, (SELECT name from person where person.id=person_visits.person_id) as person_name from person_visits
