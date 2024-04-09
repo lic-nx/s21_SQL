9a3f33a28835998e3c0cf09f6c439b8b3696edb9
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:20:37 +0300

    Update day01_ex07.sql

--- day-01/ex07/day01_ex07.sql
+++ day-01/ex07/day01_ex07.sql
@@ -1,3 +1,7 @@
+--Please write a SQL statement which returns the date of order from the person_order table and corresponding person name (name and age are formatted as in the data sample below) which made an order from the person table. Add a sort by both columns in ascending mode.
+
+
+
 SELECT person_order.order_date, name ||'(возраст:'||person.age||')' as "person_information" from person_order
     join person on person_order.person_id = person.id
 ORDER BY order_date, person_information;
