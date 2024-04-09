8b19c90b400ee291359f41b2f467b0a30b8ccaf9
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:19:12 +0300

    Update day01_ex02.sql

--- day-01/ex02/day01_ex02.sql
+++ day-01/ex02/day01_ex02.sql
@@ -1,1 +1,3 @@
+--Please write a SQL statement which returns unique pizza names from the menu table and orders by pizza_name column in descending mode. Please pay attention to the Denied section.
+
 SELECT DISTINCT pizza_name from menu order by pizza_name DESC;
