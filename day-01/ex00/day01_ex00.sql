5337f3c23db11872aa8acf80f271115eab8a6a1d
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:18:20 +0300

    Update day01_ex00.sql

--- day-01/ex00/day01_ex00.sql
+++ day-01/ex00/day01_ex00.sql
@@ -1,3 +1,6 @@
+-- Please write a SQL statement which returns menu’s identifier and pizza names from menu table and person’s identifier and person name from person table in one global list (with column names as presented on a sample below) ordered by object_id and then by object_name columns.
+
+
 SELECT id AS object_id, pizza_name as object_name from menu
 union
 SELECT id as object_id, name as object_name  from person
