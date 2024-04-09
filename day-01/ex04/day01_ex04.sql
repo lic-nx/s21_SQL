a53984df8ff4a748b16f838600cc5639fe313746
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:19:49 +0300

    Update day01_ex04.sql

--- day-01/ex04/day01_ex04.sql
+++ day-01/ex04/day01_ex04.sql
@@ -1,3 +1,6 @@
+--Please write a SQL statement which returns a difference (minus) of person_id column values with saving duplicates between person_order table and person_visits table for order_date and visit_date are for 7th of January of 2022
+
+
 (SELECT person_id from person_order where order_date = '2022-01-07')
                  EXCEPT all
 (Select person_id from person_visits where visit_date = '2022-01-07');
