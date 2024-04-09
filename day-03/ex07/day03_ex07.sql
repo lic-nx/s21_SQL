572afcc34063b38aca25f18f252f935bef1e2f39
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:36:49 +0300

    Update day03_ex07.sql

--- day-03/ex07/day03_ex07.sql
+++ day-03/ex07/day03_ex07.sql
@@ -1,2 +1,4 @@
+-- Please register a new pizza with name “greek pizza” (use id = 19) with price 800 rubles in “Dominos” restaurant (pizzeria_id = 2).
+Warning: this exercise will probably be the cause  of changing data in the wrong way. Actually, you can restore the initial database model with data from the link in the “Rules of the day” section.
 INSERT INTO menu
 values (19,(Select id from pizzeria where name = 'Dominos'), 'greek pizza', 800 );
