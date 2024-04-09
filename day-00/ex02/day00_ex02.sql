c472ea030b00047f3cb10503522922468161e5ee
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:13:59 +0300

    Update day00_ex02.sql

--- day-00/ex02/day00_ex02.sql
+++ day-00/ex02/day00_ex02.sql
@@ -1,3 +1,7 @@
+--Please make 2 syntax different select statements which return a list of pizzerias (pizzeria name and rating) with rating between 3.5 and 5 points (including limit points) and ordered by pizzeria rating.
+
+-- the 1st select statement must contain comparison signs  (<=, >=)
+-- the 2nd select statement must contain BETWEEN keyword
 SELECT name, rating from pizzeria where rating between 3.5 and 5 order by rating;
 
 SELECT name, rating from pizzeria where rating>= 3.5 and rating<=5 order by rating;
