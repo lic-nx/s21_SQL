6baf9d7bc87ff16b5ed63c1e3e842b03def2235c
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:17:11 +0300

    Update day00_ex08.sql

--- day-00/ex08/day00_ex08.sql
+++ day-00/ex08/day00_ex08.sql
@@ -1,1 +1,3 @@
+
+--Please make a SQL statement which returns all columns from the person_order table with rows whose identifier is an even number. The result have to order by returned identifier.
 SELECT id, person_id, menu_id, order_date from person_order where (id%2) = 0 ORDER BY id ;
