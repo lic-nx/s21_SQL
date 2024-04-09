c0a4562caca49646979554b75cccbbdf13feafcd
Anastasiya <58188813+lic-nx@users.noreply.github.com>
2024-04-09 10:20:55 +0300

    Update day01_ex08.sql

--- day-01/ex08/day01_ex08.sql
+++ day-01/ex08/day01_ex08.sql
@@ -1,3 +1,5 @@
+-- Please rewrite a SQL statement from exercise #07 by using NATURAL JOIN construction. The result must be the same like for exercise #07.
+
 SELECT person_order.order_date , name ||'(age:'||person.age||')' as "person_information" from person_order
    natural join (select person.id as person_id, name, age from person) person
 ORDER BY 1, 2;
